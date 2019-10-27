8eacf63d9d3b60c566209464641e0e798b7d1857
MinhGiang <minhgiangeit@gmail.com>
2019-10-28 02:39:27 +0700

    Add project files.

---
+++ ScriptReference/Controllers/ScriptController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ScriptReference.Properties;
+
+namespace ScriptReference.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ScriptController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok(Resources.Script);
+        }
+    }
+}
